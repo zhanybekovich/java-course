008a30a707b9511c68dc6aaa508adf342119722f
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-12 17:04:51 +0600

    variables and basic data types

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,38 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // This is a comment
+        // integer
+        int age = 25;
+        System.out.println(age);
 
-        /*
-            This is a
-            multiline
-            comment
-         */
+        int year = 2025;
+        System.out.println("The year is " + year);
 
-        System.out.println("Hello Java World!");
-        System.out.print("I like pizza.\n");
-        System.out.print("Hello again!");
-        
+        // double
+        double price = 19.99;
+        System.out.println("The price is " + price);
+
+        double temperature = -12.5;
+        System.out.println("The temperature is " + temperature);
+
+        // char
+        char grade = 'B';
+        System.out.println("The grade is " + grade);
+
+        // boolean
+        boolean isStudent = false;
+        System.out.println(isStudent);
+
+        boolean isMarried = true;
+        System.out.println(isMarried);
+
+        // string
+        String name = "James";
+        System.out.println("Your name is " + name);
+
+        // string concatenation
+        String firstname = "John";
+        String lastname = "Doe";
+        System.out.println(firstname + " " + lastname);
     }
 }
