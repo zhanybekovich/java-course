fe846a02bd4c71262c9b589020e4bdae197699ee
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-12 12:52:24 +0600

    hello world

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // This is a comment
+
+        /*
+            This is a
+            multiline
+            comment
+         */
+
+        System.out.println("Hello Java World!");
+        System.out.print("I like pizza.\n");
+        System.out.print("Hello again!");
+        
+    }
+}
