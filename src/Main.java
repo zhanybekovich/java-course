56f59575d2b293d19b1ecb59ef43dc52682ac5a6
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-15 09:53:22 +0600

    generating random values

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,26 @@
-import java.util.Scanner;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        // Shopping Cart Program
-        Scanner scanner = new Scanner(System.in);
+        // Generating random values
 
-        String item;
-        double price;
-        int quantity;
-        char currency = '$';
-        double total;
+        Random random = new Random();
 
-        System.out.print("What item would you like to buy?: ");
-        item = scanner.nextLine();
+        // random integer
+        int number;
+        number = random.nextInt();
 
-        System.out.print("What is the price for each?: ");
-        price = scanner.nextDouble();
+        // range from 1 to 6
+        int myRandom = random.nextInt(1, 7);
 
-        System.out.print("How many would you like?: ");
-        quantity = scanner.nextInt();
+        // random double (0 --- 1)
+        double myDouble = random.nextDouble();
 
-        total = price * quantity;
+        // random boolean
+        boolean myBoolean = random.nextBoolean();
 
-        System.out.println("\nYou have bought " + quantity + " " + item + "/s");
-        System.out.println("Your total is " + currency + total);
-
-        scanner.close();
+        System.out.println(myBoolean);
     }
 }
