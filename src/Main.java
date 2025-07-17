35494ab867472e6554cf4bad3e9de4d2e53b5ab9
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-17 17:46:46 +0600

    ternary operator

--- src/Main.java
+++ src/Main.java
@@ -4,39 +4,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Weight Conversion Program
+        // Ternary operator
+        int score = 140;
 
-        Scanner sc = new Scanner(System.in);
+        String passOrFail = (score >= 60) ? "PASS" : "FAIL";
+        System.out.println(passOrFail);
 
-        double weight;
-        double newWeight;
-        int choice;
+        int number = 4;
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+        System.out.println(evenOrOdd);
 
-        System.out.println("Weight Conversion Program");
-        System.out.println("1: Convert lbs to kgs");
-        System.out.println("2: Convert kgs to lbs");
+        int hours = 22;
+        String timeOfDay = (hours < 12) ? "AM" : "PM";
+        System.out.println(timeOfDay);
 
-        System.out.print("Choose an option: ");
-        choice = sc.nextInt();
-
-        if (choice == 1) {
-            System.out.print("Please enter the weight in lbs: ");
-            weight = sc.nextDouble();
-
-            newWeight = weight * 0.453592;
-
-            System.out.printf("The new weight in kgs is %.2f ", newWeight);
-        } else if (choice == 2) {
-            System.out.print("Please enter the weight in kgs: ");
-            weight = sc.nextDouble();
-
-            newWeight = weight * 2.20462;
-
-            System.out.printf("The new weight in lbs is %.2f ", newWeight);
-        } else {
-            System.out.println("Invalid choice");
-        }
-
-        sc.close();
+        int income = 60000;
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.println(taxRate);
     }
 }
