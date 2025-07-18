7ea6b0c4eff6919b8c608284522fd8bc63056424
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-18 10:40:00 +0600

    simple calculator program

--- src/Main.java
+++ src/Main.java
@@ -4,31 +4,47 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // Enhanced switch = a replacement to many else if statements
-
+        // Simple Calculator Project
         Scanner sc = new Scanner(System.in);
-        String day;
 
-        System.out.print("Enter the day of the week: ");
-        day = sc.nextLine();
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
 
-//        switch (day) {
-//            case "Monday" -> System.out.println("It is a weekday 😭");
-//            case "Tuesday" -> System.out.println("It is a weekday 😭");
-//            case "Wednesday" -> System.out.println("It is a weekday 😭");
-//            case "Thursday" -> System.out.println("It is a weekday 😭");
-//            case "Friday" -> System.out.println("It is a weekday 😭");
-//            case "Saturday" -> System.out.println("It is a weekend 😛");
-//            case "Sunday" -> System.out.println("It is a weekend 😛");
-//            default -> System.out.println("Invalid day");
-//        }
+        System.out.print("Enter the first number: ");
+        num1 = sc.nextDouble();
 
-        switch (day) {
-            case "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" -> System.out.println("It is a weekday 😭");
-            case "Saturday", "Sunday" -> System.out.println("It is a weekend 😛");
-            default -> System.out.println("Invalid day");
+        System.out.print("Enter an operator (+, -, *, /, ^: ");
+        operator = sc.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = sc.nextDouble();
+
+        switch (operator) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if (num2 == 0) {
+                    System.out.println("Cannot divide by zero!");
+                    validOperation = false;
+                } else {
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid operator!");
+                validOperation = false;
+            }
         }
 
+        if (validOperation) {
+            System.out.printf("The result is %.2f", result);
+        }
+        
         sc.close();
     }
 }
