2a8b689e269d0f1c540e0cc828206c261ef09312
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-19 18:09:22 +0600

    dice rolling game

--- src/Main.java
+++ src/Main.java
@@ -1,83 +1,87 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        // Dice Rolling Game
+        Scanner sc = new Scanner(System.in);
 
-        // Banking program
+        Random random = new Random();
+        int numOfDice;
+        int total = 0;
 
-        // declare vars
-        double balance = 0;
-        boolean isRunning = true;
-        int choice;
+        System.out.print("Enter the number of dice to roll: ");
+        numOfDice = sc.nextInt();
 
-        // display menu
-        while (isRunning) {
-            System.out.println("***************");
-            System.out.println("BANKING PROGRAM");
-            System.out.println("***************");
-            System.out.println("1. Show Balance");
-            System.out.println("2. Deposit");
-            System.out.println("3. Withdraw");
-            System.out.println("4. Exit");
-            System.out.println("***************");
-
-            // get and process user's choice
-            System.out.print("Enter your choice (1-4): ");
-            choice = sc.nextInt();
-
-            switch (choice) {
-                case 1 -> showBalance(balance);
-                case 2 -> balance += deposit();
-                case 3 -> balance -= withdraw(balance);
-                case 4 -> isRunning = false;
-                default -> System.out.println("Invalid choice!");
+        if (numOfDice > 0) {
+            for (int i = 0; i < numOfDice; i++) {
+                int roll = random.nextInt(1, 7);
+                printDie(roll);
+                System.out.println(roll);
+                total = total + roll;
             }
+        } else {
+            System.out.println("Number of dice must be positive");
         }
 
-        // exit message
-        System.out.println("***************");
-        System.out.println("Thank you! Have a nice day!");
-        System.out.println("***************");
 
         sc.close();
     }
 
-    static void showBalance(double balance) {
-        System.out.println("***************");
-        System.out.printf("$%.2f\n", balance);
-    }
+    static void printDie(int roll) {
+        String dice1 = """
+                 -------
+                |       |
+                |   ●   |
+                |       |
+                 -------
+                """;
+        String dice2 = """
+                 -------
+                | ●     |
+                |       |
+                |     ● |
+                 -------
+                """;
+        String dice3 = """
+                 -------
+                | ●     |
+                |   ●   |
+                |     ● |
+                 -------
+                """;
+        String dice4 = """
+                 -------
+                | ●   ● |
+                |       |
+                | ●   ● |
+                 -------
+                """;
+        String dice5 = """
+                 -------
+                | ●   ● |
+                |   ●   |
+                | ●   ● |
+                 -------
+                """;
+        String dice6 = """
+                 -------
+                | ●   ● |
+                | ●   ● |
+                | ●   ● |
+                 -------
+                """;
 
-    static double deposit() {
-        double amount;
-
-        System.out.print("Enter an amount to be deposited: ");
-        amount = sc.nextDouble();
-
-        if (amount < 0) {
-            System.out.println("Amount can't be negative!");
-            return 0;
-        } else {
-            return amount;
-        }
-    }
-
-    static double withdraw(double balance) {
-
-        double amount;
-        System.out.print("Enter an amount to be withdrawn: ");
-        amount = sc.nextDouble();
-
-        if (amount > balance) {
-            System.out.println("Insufficient funds!");
-            return 0;
-        } else if (amount < 0) {
-            System.out.println("Amount can't be negative!");
-            return 0;
-        } else {
-            return amount;
+        switch (roll) {
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.println("Invalid roll");
         }
     }
 
