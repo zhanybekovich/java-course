9bb082ea13730b53bee66a498d44f6a02312620d
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-19 14:50:54 +0600

    methods in java

--- src/Main.java
+++ src/Main.java
@@ -4,38 +4,51 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // nested loops
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 10; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
+        String name = "John";
+        int age = 20;
+
+        happyBirthday(name, age);
+        happyBirthday("Bob", 10);
+
+        System.out.println(square(3));
+        System.out.println(cube(3));
+
+        System.out.println(getFullName("John", "Doe"));
+
+        if (isAdult(19)) {
+            System.out.println("You can get a credit card!");
+        } else {
+            System.out.println("You can not get a credit card!");
         }
 
-        int rows;
-        int cols;
-        char symbol;
+    }
 
-        Scanner sc = new Scanner(System.in);
+    // method - a block of reusable code that is executed when called()
+    static void happyBirthday(String name, int age) {
+        System.out.println("Happy Birthday to you!");
+        System.out.printf("Happy Birthday dear %s! \n", name);
+        System.out.printf("You are %d years old!\n", age);
+        System.out.println("Happy Birthday to you!\n");
+    }
 
-        System.out.print("Enter the number of rows: ");
-        rows = sc.nextInt();
+    // methods returning data
+    static double square(double number) {
+        return number * number;
+    }
 
-        System.out.print("Enter the number of columns: ");
-        cols = sc.nextInt();
+    static double cube(double number) {
+        return number * number * number;
+    }
 
-        System.out.print("Enter the symbol to use: ");
-        symbol = sc.next().charAt(0);
+    static String getFullName(String firstName, String lastName) {
+        return firstName + " " + lastName;
+    }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                System.out.print(symbol);
-            }
-            System.out.println();
+    static boolean isAdult(int age) {
+        if (age >= 18) {
+            return true;
+        } else {
+            return false;
         }
-
-
-        sc.close();
-
     }
 }
