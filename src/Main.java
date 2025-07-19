a0d6c6ca5ba945cc4b18a32131dd66f84c80480a
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-19 16:18:50 +0600

    banking program

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,83 @@ import java.util.Scanner;
 
 public class Main {
 
-    // class variable
-    static int y = 3;
+    static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
 
-        // variable scope = where a variable can be accessed
+        // Banking program
 
-        // local scope
-        int x = 1;
-        System.out.println(x);
+        // declare vars
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
 
-        doSomething();
+        // display menu
+        while (isRunning) {
+            System.out.println("***************");
+            System.out.println("BANKING PROGRAM");
+            System.out.println("***************");
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println("***************");
 
-        // can access class variable
-        System.out.println(y);
-        doSomething2();
+            // get and process user's choice
+            System.out.print("Enter your choice (1-4): ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> balance -= withdraw(balance);
+                case 4 -> isRunning = false;
+                default -> System.out.println("Invalid choice!");
+            }
+        }
+
+        // exit message
+        System.out.println("***************");
+        System.out.println("Thank you! Have a nice day!");
+        System.out.println("***************");
+
+        sc.close();
     }
 
-    static void doSomething() {
-        // local
-        int x = 2;
-
-        System.out.println(x);
+    static void showBalance(double balance) {
+        System.out.println("***************");
+        System.out.printf("$%.2f\n", balance);
     }
 
-    static void doSomething2() {
-        System.out.println(y);
+    static double deposit() {
+        double amount;
+
+        System.out.print("Enter an amount to be deposited: ");
+        amount = sc.nextDouble();
+
+        if (amount < 0) {
+            System.out.println("Amount can't be negative!");
+            return 0;
+        } else {
+            return amount;
+        }
+    }
+
+    static double withdraw(double balance) {
+
+        double amount;
+        System.out.print("Enter an amount to be withdrawn: ");
+        amount = sc.nextDouble();
+
+        if (amount > balance) {
+            System.out.println("Insufficient funds!");
+            return 0;
+        } else if (amount < 0) {
+            System.out.println("Amount can't be negative!");
+            return 0;
+        } else {
+            return amount;
+        }
     }
 
 }
