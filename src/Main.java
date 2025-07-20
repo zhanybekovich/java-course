c5e0d41a8fbcc45241a9c938fcbc99945d6b7ade
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-07-20 19:12:54 +0600

    task 1.3 solution

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,13 @@ public class Main {
     public static void main(String[] args) {
 
         /*
-        1.2 Print the numbers 47, 52, and 150 on one line with two spaces between them.
-        Do not use the text '47 52 150'.
+        1.3 Display the numbers 50 and 10 one below the other.
         */
 
-        int a = 47;
-        int b = 52;
-        int c = 150;
+        int a = 50;
+        int b = 10;
 
-        System.out.printf("%d  %d  %d", a, b, c);
+        System.out.println(a);
+        System.out.println(b);
     }
 }
