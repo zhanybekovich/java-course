dfc159fec22a72f96c6a832014a0eefde8c5ea78
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-06 21:02:25 +0600

    clear

--- src/Cat.java
+++
@@ -1,7 +0,0 @@
-public class Cat extends Animal {
-
-    @Override
-    void speak() {
-        System.out.println("The cat goes meow");
-    }
-}
--- src/Dog.java
+++
@@ -1,7 +0,0 @@
-public class Dog extends Animal {
-
-    @Override
-    void speak() {
-        System.out.println("The dog goes woof");
-    }
-}
