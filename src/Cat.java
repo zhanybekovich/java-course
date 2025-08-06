7e8794dc6488a1ac05502173b1724b64ce80a6d7
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-08-06 21:01:29 +0600

    runtime polymorphism

---
+++ src/Cat.java
@@ -0,0 +1,7 @@
+public class Cat extends Animal {
+
+    @Override
+    void speak() {
+        System.out.println("The cat goes meow");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,28 @@
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        // Runtime polymorphism = when the method that gets executed is decided
+        // at runtime based on the actual type of the object.
+
+        Animal animal;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Would you like a dog or a cat? (1 - Dog, 2 - Cat): ");
+        int choice = sc.nextInt();
+
+        if (choice == 1) {
+            animal = new Dog();
+            animal.speak();
+        } else if (choice == 2) {
+            animal = new Cat();
+            animal.speak();
+        }
+
+        sc.close();
 
     }
 
